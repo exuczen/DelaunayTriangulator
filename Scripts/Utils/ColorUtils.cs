9f7ef9fc57727375191e7e15f78127bc081adbbc
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-03-30 17:55:09 +0200

    Added Color32FromARGB.

--- Scripts/Utils/ColorUtils.cs
+++ Scripts/Utils/ColorUtils.cs
@@ -7,6 +7,15 @@ namespace Triangulation
     public static class ColorUtils
     {
 #if UNITY
+        public static UnityEngine.Color32 Color32FromARGB(int argb)
+        {
+            byte b = (byte)(argb >>= 0 & 0xff);
+            byte g = (byte)(argb >>= 8 & 0xff);
+            byte r = (byte)(argb >>= 8 & 0xff);
+            byte a = (byte)(argb >>= 8 & 0xff);
+            return new UnityEngine.Color32(r, g, b, a);
+        }
+
         public static UnityEngine.Color32 ToColor32(this System.Drawing.Color c)
         {
             return new UnityEngine.Color32(c.R, c.G, c.B, c.A);
