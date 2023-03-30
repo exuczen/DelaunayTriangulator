4597420fb8f392ef2239b32854482a039188f60b
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-03-30 18:12:10 +0200

    Added color utility methods.

--- Scripts/Utils/ColorUtils.cs
+++ Scripts/Utils/ColorUtils.cs
@@ -2,6 +2,9 @@
 #define UNITY
 #endif
 
+using System.Drawing;
+using System;
+
 namespace Triangulation
 {
     public static class ColorUtils
@@ -16,10 +19,46 @@ namespace Triangulation
             return new UnityEngine.Color32(r, g, b, a);
         }
 
-        public static UnityEngine.Color32 ToColor32(this System.Drawing.Color c)
+        public static UnityEngine.Color32 ToColor32(this Color c)
         {
             return new UnityEngine.Color32(c.R, c.G, c.B, c.A);
         }
 #endif
+
+        public static Color ColorToColor(DeprecatedColor color)
+        {
+            return Color.FromArgb(color.argb);
+        }
+
+        public static Color ColorFromHSV(float hue, float saturation, float value)
+        {
+            int hi = Convert.ToInt32(MathF.Floor(hue / 60f)) % 6;
+            float f = hue / 60f - MathF.Floor(hue / 60f);
+
+            value *= 255f;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1f - saturation));
+            int q = Convert.ToInt32(value * (1f - f * saturation));
+            int t = Convert.ToInt32(value * (1f - (1f - f) * saturation));
+
+            return hi switch {
+                0 => Color.FromArgb(255, v, t, p),
+                1 => Color.FromArgb(255, q, v, p),
+                2 => Color.FromArgb(255, p, v, t),
+                3 => Color.FromArgb(255, p, q, v),
+                4 => Color.FromArgb(255, t, p, v),
+                _ => Color.FromArgb(255, v, p, q),
+            };
+        }
+
+        public static void ColorToHSV(Color color, out float hue, out float saturation, out float value)
+        {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            hue = color.GetHue();
+            saturation = (max == 0) ? 0 : 1f - (1f * min / max);
+            value = max / 255f;
+        }
     }
 }
