464239983b99c38e9351b15b2cccf7d200802370
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-03-30 17:40:38 +0200

    Added ColorUtils.

---
+++ Scripts/Utils/ColorUtils.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR || UNITY_STANDALONE
+#define UNITY
+#endif
+
+namespace Triangulation
+{
+    public static class ColorUtils
+    {
+#if UNITY
+        public static UnityEngine.Color32 ToColor32(this System.Drawing.Color c)
+        {
+            return new UnityEngine.Color32(c.R, c.G, c.B, c.A);
+        }
+#endif
+    }
+}
