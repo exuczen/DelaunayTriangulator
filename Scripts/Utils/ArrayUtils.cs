29ff331952db5d69b2f51899140ef6c1c4925236
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-04-06 23:54:43 +0200

    Fixed CutRange in ArrayUtils.

--- Scripts/Utils/ArrayUtils.cs
+++ Scripts/Utils/ArrayUtils.cs
@@ -8,16 +8,26 @@ namespace Triangulation
         {
             int length = range.FullLength;
             int count = length - range.GetIndexCount();
+            if (count <= 0)
+            {
+                return 0;
+            }
             int beg = range.Beg;
             int end = range.End;
+            int last = length - 1;
+
             if (end < beg || beg == 0)
             {
                 Array.Copy(srcArray, end + 1, destArray, 0, count);
             }
-            else if (end < length - 1)
+            else if (end < last)
             {
-                Array.Copy(srcArray, 0, destArray, length, beg);
-                Array.Copy(srcArray, end + 1, destArray, 0, count);
+                Array.Copy(srcArray, end + 1, destArray, 0, last - end);
+                Array.Copy(srcArray, 0, destArray, last - end, beg);
+            }
+            else
+            {
+                Array.Copy(srcArray, 0, destArray, 0, beg);
             }
             return count;
         }
