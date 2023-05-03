8032e875ab710f483445b953fb8c16cb5944ae02
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-05-03 20:05:24 +0200

    Added Bounds2Int, GridPointsXYComparer, GridPointsYXComparer.

---
+++ Scripts/Geometry/Bounds2Int.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace Triangulation
+{
+    public struct Bounds2Int : IEquatable<Bounds2Int>
+    {
+        public Vector2Int Size => max - min + Vector2Int.One;
+
+        public Vector2Int min;
+        public Vector2Int max;
+
+        public static Bounds2Int MinMax(Vector2Int min, Vector2Int max)
+        {
+            return new Bounds2Int
+            {
+                min = min,
+                max = max
+            };
+        }
+
+        public static Bounds2Int GetBounds(Vector2Int[] pointsXY, int beg, int end)
+        {
+            if (beg < 0 || end < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            var min = pointsXY[beg];
+            var max = min;
+
+            for (int i = beg + 1; i <= end; ++i)
+            {
+                Vector2Int v = pointsXY[i];
+                max = Vector2Int.Max(max, v);
+                min = Vector2Int.Min(min, v);
+            }
+            return MinMax(min, max);
+        }
+
+        public Bounds2Int(Vector2Int min, Vector2Int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public bool Contains(Vector2Int pos)
+        {
+            return pos.x >= min.x && pos.x <= max.x && pos.y >= min.y && pos.y <= max.y;
+        }
+
+        public bool Contains(Vector2 pos)
+        {
+            return pos.x >= min.x && pos.x <= max.x && pos.y >= min.y && pos.y <= max.y;
+        }
+
+        public bool Equals(Bounds2Int other)
+        {
+            return min == other.min && max == other.max;
+        }
+    }
+}
---
+++ Scripts/Geometry/GridPointsXYComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Triangulation
+{
+    public struct GridPointXYComparer : IComparer<Vector2Int>
+    {
+        public int Compare(Vector2Int p1, Vector2Int p2)
+        {
+            int delta = p1.x - p2.x;
+            if (delta != 0)
+            {
+                return Math.Sign(delta);
+            }
+            delta = p1.y - p2.y;
+            if (delta != 0)
+            {
+                return Math.Sign(delta);
+            }
+            return 0;
+        }
+    }
+}
